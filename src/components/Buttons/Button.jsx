8cbaabfcbb5970096b4fe4394e60543da3f632be
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-08-25 02:26:08 +0300

    add another buttons

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -67,3 +67,39 @@ export const ButtonWithoutIcon = ({ title }) => {
     </Button>
   );
 };
+
+export const ButtonsAdd = () => {
+  return (
+    <Button
+      variant="contained"
+      disableElevation
+      sx={{
+        display: 'flex',
+        color: '#FFFFFF',
+        fontFamily: 'Poppins, sans-serif',
+        alignItems: 'center',
+        textTransform: 'none',
+        borderRadius: '8px',
+        marginTop: '40px',
+        letterSpacing: '-0.02em',
+        fontSize: '14px',
+        background: '#121212',
+        lineHeight: '21px',
+        fontWeight: '500',
+        fontStyle: 'normal',
+        width: '334px',
+        height: '56px',
+        '&:hover': {
+          background: '#121212',
+        },
+      }}
+      startIcon={
+        <AddIcon
+          sx={{ background: '#FFFFFF', borderRadius: '6px', color: '#121212' }}
+        />
+      }
+    >
+      Add another column
+    </Button>
+  );
+};
