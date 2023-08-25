6d4ae38cb4308fc667cd9d37f6cfe115719ab1c3
SerhiiRepinskyi <repinskyisv@gmail.com>
2023-08-25 17:19:59 +0000

    Deploying to gh-pages from @ SerhiiRepinskyi/project-future777-frontend@86f8e79049e6116c0421aba6fce8cd88ec86b6b3 🚀

---
+++ static/js/123.3907f210.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[123],{123:function(e,t,c){c.r(t);var r=c(184);t.default=function(){return(0,r.jsx)("section",{children:(0,r.jsx)("h1",{children:"WelcomePage"})})}}}]);
+//# sourceMappingURL=123.3907f210.chunk.js.map
---
+++ static/js/786.7eb080ec.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[786],{786:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("section",{children:(0,c.jsx)("h1",{children:"AuthPage"})})}}}]);
+//# sourceMappingURL=786.7eb080ec.chunk.js.map
