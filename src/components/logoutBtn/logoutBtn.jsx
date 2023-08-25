302991d79b1c8a427844274f35521b0528d54e17
AShvab <shvabovska@gmail.com>
2023-08-25 09:06:16 +0300

    add base button logout

---
+++ src/components/logoutBtn/logoutBtn.jsx
@@ -0,0 +1,33 @@
+// import { useDispatch } from "react-redux";
+import sprite from "../../assets/images/sprite.svg";
+import { Icon, LogOutBtnStyled, SpanStyled } from "./logoutBtn.styled";
+// import { setToken, useLogoutMutation} from "redux/authSlice"
+// import { getToken } from 'redux/selectors';
+
+
+const LogoutBtn = () => {    
+    // const dispatch = useDispatch();
+    // const token = useSelector(getToken);
+    // const [logOut] = useLogoutMutation();
+
+    const handleLogout = () => {
+        console.log("LogoutBtn clicked");  
+        // dispatch(setToken('')); 
+      }
+
+    return (
+        <>
+      {/* <LogOutBtnStyled onClick={() => dispatch(logOut())}> */}
+      <LogOutBtnStyled onClick={handleLogout}>
+        <Icon width="32px" height="32px">
+          <use href={`${sprite}#icon-logout`} />
+        </Icon>
+        <SpanStyled>Log out</SpanStyled>
+      </LogOutBtnStyled>
+        </>
+    )
+
+}
+
+export default LogoutBtn;
+
---
+++ src/components/logoutBtn/logoutBtn.styled.js
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+
+export const LogOutBtnStyled = styled.button`
+  display: flex;
+  gap: 14px;
+  padding: 0px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 24px;
+  height: 32px;
+  width: 105px;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const SpanStyled = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+  color: #161616;
+  font-family: 'Poppins, sans-serif';
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+export const Icon = styled.svg`
+  width: 32px;
+  height: 32px;
+  fill: none;
+  stroke: #bedbb0;
+  &:hover {
+    stroke: #9dc888;
+  }
+
+  &:focus {
+    stroke: #9dc888;
+  }
+`;
+
+
