dff47ea97fd9f29570b04067e0f8da8a30d693b6
AShvab <shvabovska@gmail.com>
2023-08-26 17:29:04 +0300

    changed routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,59 @@
-import { NotFound } from 'pages/NotFound/NotFound';
+// import { Suspense, lazy } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Loader from './Loader/Loader';
+
+// const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
+// const HomePage = lazy(() => import ('../pages/HomePage/HomePage'));
+// const AuthPage = lazy(() => import('../pages/AuthPage'));
+// const ScreensPage = lazy(() => import ('../pages/ScreensPage/ScreensPage'));
+// const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Suspense fallback={<Loader />}>
+//         <Routes>
+//           <Route path="/welcome" element={<WelcomePage />} index />
+//           <Route
+//             path="/auth/:id"
+//             element={/*<RestrictedRoute>*/ <AuthPage /> /*<RestrictedRoute>*/}
+//           />
+//           <Route
+//             path="/home"
+//             element={/*<PrivateRoute>*/ <HomePage /> /*<PrivateRoute>*/}
+//           />
+//           <Route
+//             path="/home/:boardName"
+//             element={/*<PrivateRoute>*/ <ScreensPage /> /*<PrivateRoute>*/}
+//           />
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </Suspense>
+//     </div>
+//   );
+// };
+
 import { Suspense, lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
+import Loader from './Loader/Loader';
+import SharedLayout from 'SharedLayout/SharedLayout';
 
 const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
 const HomePage = lazy(() => import ('../pages/HomePage/HomePage'));
 const AuthPage = lazy(() => import('../pages/AuthPage'));
 const ScreensPage = lazy(() => import ('../pages/ScreensPage/ScreensPage'));
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
 
 
 export const App = () => {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/" element={<SharedLayout />} />
+        <Route index element={<WelcomePage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
           <Route
             path="/auth/:id"
             element={/*<RestrictedRoute>*/ <AuthPage /> /*<RestrictedRoute>*/}
@@ -27,8 +67,13 @@ export const App = () => {
             element={/*<PrivateRoute>*/ <ScreensPage /> /*<PrivateRoute>*/}
           />
           <Route path="*" element={<NotFound />} />
+       
+          
         </Routes>
       </Suspense>
     </div>
   );
 };
+
+
+
