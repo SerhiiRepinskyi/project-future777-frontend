099350ecf904d32ab8a8183baf3f8964b2e16404
AShvab <shvabovska@gmail.com>
2023-08-28 12:51:09 +0300

    styled page 404

--- src/pages/NotFound/NotFound.styled.js
+++ src/pages/NotFound/NotFound.styled.js
@@ -1,9 +1,56 @@
 import styled from 'styled-components';
 
+
+export const Container = styled.div`
+height: 100vh;
+  padding: 20px;
+  background: linear-gradient(
+    180deg,
+    rgba(196, 196, 196, 0) 25%,
+    #bedbb0 92.19%
+  );
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-left: auto;
+  margin-right: auto;
+`
 export const Title = styled.h1`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   text-align: center;
+  font-size:16px;
+  margin-bottom: 0;
+
+@media screen and (min-width: 768px) {
+  font-size: 28px;
+}
+@media screen and (min-width: 1440px) {
+  font-size: 40px;
+}
 `;
+
+export const Text = styled.p`
+  text-align: center;
+  font-size:12px;
+  margin-bottom: 0;
+
+@media screen and (min-width: 768px) {
+  font-size: 20px;
+}
+@media screen and (min-width: 1440px) {
+  font-size: 28px;
+}
+`;
+
+export const Image = styled.img`
+  width: 200px;
+
+@media screen and (min-width: 768px) {
+  width: 300px;
+}
+@media screen and (min-width: 1440px) {
+  width: 430px;
+}
+`
