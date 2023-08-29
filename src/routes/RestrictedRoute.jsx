df0b314579edd71206e069b36e5146da4171f0dc
AShvab <shvabovska@gmail.com>
2023-08-30 00:14:41 +0300

    changed routes

--- src/routes/RestrictedRoute.jsx
+++ src/routes/RestrictedRoute.jsx
@@ -1,11 +1,11 @@
-import { Navigate} from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { Navigate} from "react-router-dom";
+// import { useSelector } from "react-redux";
 
 
 const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
-  const isLoggedIn = useSelector(state => state.auth.token);
+  // const isLoggedIn = useSelector(state => state.auth.token);
 
-  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+  // return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
 
 export default RestrictedRoute;
