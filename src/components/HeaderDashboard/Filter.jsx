420809e4377fd3efef8de3ed99bc2168b25f43ab
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-09-01 02:40:23 +0300

    modal

--- src/components/HeaderDashboard/Filter.jsx
+++ src/components/HeaderDashboard/Filter.jsx
@@ -6,7 +6,11 @@ import { useState } from 'react';
 
 import { arrBG } from '../ModalBoard/data';
 import { Button, Div, Label, LabelGroup, RadioWrap } from './Filter.styled';
-import { LiStyled, UlBgStyled } from 'components/ModalBoard/ModalBoard.styled';
+import {
+  ImgStyled,
+  LiStyled,
+  UlBgStyled,
+} from 'components/ModalBoard/ModalBoard.styled';
 
 const titleStyle = {
   color: '#FFF',
@@ -18,8 +22,139 @@ const titleStyle = {
   // marginBottom: 24 / 8,
 };
 
+// export const Filter = ({ open, onClose }) => {
+//   const [bgImgIndex, setBgImgIndex] = useState(null);
+
+//   const [currentPrority, setCurrentPrority] = useState('');
+//   const [isShowAllActive, setIsShowAllActive] = useState(false);
+//   const initialValues = {
+//     currentBg: 0,
+//     currentPrority: '',
+//   };
+
+//   const controlProps = item => ({
+//     checked: currentPrority === item,
+//     onChange: handleChange,
+//     value: item,
+//   });
+//   const handleChange = event => {
+//     const filter = event.target.value;
+//     setCurrentPrority(filter);
+//     // dispatch(setFilterCards(filter));
+//     setIsShowAllActive(false);
+//   };
+
+//   const handleShowAll = () => {
+//     setCurrentPrority('');
+//     // dispatch(setFilterCards(''));
+//     setIsShowAllActive(true);
+//   };
+
+//   const handleRadioClick = () => {
+//     if (isShowAllActive) {
+//       setIsShowAllActive(false);
+//     }
+//   };
+//   // console.log(currentPrority);
+//   console.log(bgImgIndex);
+
+//   return (
+//     <ModalLayout title={'Filters'} open={open} handleClose={onClose}>
+//       <Formik initialValues={initialValues}>
+//         <Form onChange={handleChange}>
+//           <UlBgStyled>
+//             <LiStyled></LiStyled>
+//             {arrBG.map((el, index) => {
+//               return (
+//                 <LiStyled
+//                   key={index + 1}
+//                   onClick={() => setBgImgIndex(index + 1)}
+//                 >
+//                   <img src={el} alt="" width="24px" height="24px" />
+//                 </LiStyled>
+//               );
+//             })}
+//           </UlBgStyled>
+//           <LabelGroup role="group" aria-labelledby="my-radio-group">
+//             <Div>
+//               <Typography variant="h2" sx={titleStyle}>
+//                 Label Color
+//               </Typography>
+//               <Button
+//                 type="button"
+//                 onClick={handleShowAll}
+//                 disabled={isShowAllActive}
+//               >
+//                 Show all
+//               </Button>
+//             </Div>
+//             <RadioWrap>
+//               <Label>
+//                 <Radio
+//                   onClick={handleRadioClick}
+//                   checked={currentPrority === 'without'}
+//                   name="currentPrority"
+//                   {...controlProps('without')}
+//                   size="small"
+//                   sx={{ color: 'grey', '&.Mui-checked': { color: 'grey' } }}
+//                 />
+//                 Without priority
+//               </Label>
+//               <Label>
+//                 <Radio
+//                   onClick={handleRadioClick}
+//                   checked={currentPrority === 'low'}
+//                   name="currentPrority"
+//                   {...controlProps('low')}
+//                   size="small"
+//                   sx={{
+//                     color: '#8FA1D0',
+//                     '&.Mui-checked': { color: '#8FA1D0' },
+//                   }}
+//                 />
+//                 Low
+//               </Label>
+//               <Label>
+//                 <Radio
+//                   onClick={handleRadioClick}
+//                   checked={currentPrority === 'medium'}
+//                   name="currentPrority"
+//                   {...controlProps('medium')}
+//                   size="small"
+//                   sx={{
+//                     color: '#E09CB5',
+//                     '&.Mui-checked': { color: '#E09CB5' },
+//                   }}
+//                 />
+//                 Medium
+//               </Label>
+
+//               <Label>
+//                 <Radio
+//                   onClick={handleRadioClick}
+//                   checked={currentPrority === 'high'}
+//                   name="currentPrority"
+//                   {...controlProps('high')}
+//                   size="small"
+//                   sx={{
+//                     color: '#BEDBB0',
+//                     '&.Mui-checked': { color: '#BEDBB0' },
+//                   }}
+//                 />
+//                 High
+//               </Label>
+//             </RadioWrap>
+//           </LabelGroup>
+//         </Form>
+//       </Formik>
+//     </ModalLayout>
+//   );
+// };
+
 export const Filter = ({ open, onClose }) => {
-  const [bgImgIndex, setBgImgIndex] = useState(null);
+  // const [bgImgIndex, setBgImgIndex] = useState(null);
+  const [backgroundURL, setBackgroundURL] = useState(arrBG[0]);
+  const [backgroundIndex, setBackgroundIndex] = useState(0);
 
   const [currentPrority, setCurrentPrority] = useState('');
   const [isShowAllActive, setIsShowAllActive] = useState(false);
@@ -51,8 +186,9 @@ export const Filter = ({ open, onClose }) => {
       setIsShowAllActive(false);
     }
   };
-  // console.log(currentPrority);
-  console.log(bgImgIndex);
+  console.log(currentPrority);
+  console.log(backgroundIndex);
+  console.log(backgroundURL);
 
   return (
     <ModalLayout title={'Filters'} open={open} handleClose={onClose}>
@@ -60,13 +196,16 @@ export const Filter = ({ open, onClose }) => {
         <Form onChange={handleChange}>
           <UlBgStyled>
             <LiStyled></LiStyled>
-            {arrBG.map((el, index) => {
+            {arrBG.map((bg, index) => {
               return (
                 <LiStyled
-                  key={index + 1}
-                  onClick={() => setBgImgIndex(index + 1)}
+                  key={bg}
+                  onClick={() => {
+                    setBackgroundURL(bg);
+                    setBackgroundIndex(index);
+                  }}
                 >
-                  <img src={el} alt="" width="24px" height="24px" />
+                  <ImgStyled src={bg} alt="background picture" />
                 </LiStyled>
               );
             })}
