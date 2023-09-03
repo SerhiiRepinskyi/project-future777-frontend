442f568bf7f1734d5405d36693bf918e25d3d3ac
Vallyblink <115736687+Vallyblink@users.noreply.github.com>
2023-09-03 16:37:48 +0000

    Deploying to gh-pages from @ SerhiiRepinskyi/project-future777-frontend@86f1c793bd744ec83446d2077cf1f1c72c3e5e20 🚀

---
+++ static/js/422.5f4adbc0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[422],{9422:function(e,r,t){t.r(r);var a=t(1188),n=t(2650),s=(t(2791),t(7689)),c=t(184);r.default=function(){var e=(0,s.UO)().id;return(0,c.jsxs)(c.Fragment,{children:["register"===e&&(0,c.jsx)(n.B,{}),"login"===e&&(0,c.jsx)(a.U,{})]})}}}]);
+//# sourceMappingURL=422.5f4adbc0.chunk.js.map
