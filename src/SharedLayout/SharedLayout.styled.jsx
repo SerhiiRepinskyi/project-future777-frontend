6be77df490b2d84b4f2026740517f9ef30b3d00a
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-09-07 15:07:29 +0300

    delete unused files

--- src/SharedLayout/SharedLayout.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-//   max-width: 960px;
-//   margin: 0 auto;
-//   padding: 0 16px;
-
-// background-color: pink;
-`;
